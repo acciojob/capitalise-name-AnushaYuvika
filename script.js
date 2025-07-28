400223c20ee2297d93f8cc911dbdefd1f26fd414
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-07-28 22:45:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //your JS code here. If required.
-function upperCase (str) {
-	let s = " ";
-	for (let i=0; i<str.length; i++) {
-		s = str.toUpperCase();
-	}
-	return s; 
-
+function upperCase(str) {
+  let s = "";
+  for (let i = 0; i < str.length; i++) {
+    let ch = str[i];
+    if (ch >= 'a' && ch <= 'z') {
+      s += String.fromCharCode(ch.charCodeAt(0) - 32);
+    } else {
+      s += ch;
+    }
+  }
+  return s;
 }
-const s = prompt("Enter String: ");
-alert(upperCase(s)); 
+ 
+const s = prompt("Enter your name:");
+alert(upperCase(s));
